280e02caae2616836d62a4139bec0bba2739b194
Eduardo Fernandez <eduardo.fernandez@mercadolibre.com>
2021-01-30 16:28:50 -0300

    add: add base project

---
+++ src/test/java/com/kata/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
